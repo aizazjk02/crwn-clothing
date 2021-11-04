d941818c34b8140a54f91cb316591b54e2cfee87
Aizaz <aizazkotwal02@gmail.com>
2021-11-04 17:10:51 +0530

    broken our homepage into smaller components and finished with styling our menu items

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+// functional menuItem component 
+const MenuItem = ({ title, imageUrl,size }) => {
+  return (
+    <div
+      // Dynamic class
+      className={`${size} menu-item`}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Directory from "../../components/directory-meu/directory.component";
+import "./homepage.styles.scss";
+export const Homepage = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
