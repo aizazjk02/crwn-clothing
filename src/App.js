a4f03a162fcb504828ceca5c76fc2d318f1e9694
Aizaz <aizazkotwal02@gmail.com>
2021-11-11 16:33:41 +0530

    done wit the google authentication

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import { ShopPage } from "./pages/shop/shop.component";
 import { Header } from "./components/header/header.component";
 import { SignInAndSignUp } from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 class App extends React.Component {
   constructor() {
     super();
@@ -20,9 +20,21 @@ class App extends React.Component {
 
   // method to make sure that the user remains signed in
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) =>
-      this.setState({ currentUser: user })
-    );
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        console.log(userAuth)
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else this.setState({ currentUser: userAuth });
+    });
   }
 
   // method to clear the user data after sign out
@@ -33,7 +45,7 @@ class App extends React.Component {
     return (
       //Needs the Switch wrapper to manage the route
       <div>
-        <Header currentUser={ this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-// Config for the firebase app 
+// Config for the firebase app
 const config = {
   apiKey: "AIzaSyBRw0PxNWmaX53Jgii9aSBBusbbNVotp9o",
 
@@ -19,17 +19,38 @@ const config = {
   measurementId: "G-J1K0L42R39",
 };
 
-// initialising the firebase with the config object from the portal 
+export const createUserProfileDocument = async (userAuth, additionlData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionlData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+};
+// initialising the firebase with the config object from the portal
 firebase.initializeApp(config);
 
-// exorting the auth and firestore lib 
+// exorting the auth and firestore lib
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-// setting up the signin with google 
+// setting up the signin with google
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ promp: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase; 
-
+export default firebase;
