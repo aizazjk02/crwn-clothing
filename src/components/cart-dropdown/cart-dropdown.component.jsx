e1328a24f65176b732c9f8dc1f8f4fe36d011ff7
Aizaz <aizazkotwal02@gmail.com>
2021-11-25 23:08:33 +0530

    updating our application with selectors to avoid unnecessary rerendering

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import "./cart-dropdown.styles.scss";
 import { CustomButton } from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import CartItem from "../cart-item/cart-item";
+import { selectCartItems } from "../../redux/cart/cart-selector";
 const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
@@ -16,8 +17,8 @@ const CartDropDown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems:selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropDown);
